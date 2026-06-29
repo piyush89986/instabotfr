60f69841c9ff91c0d3d3ed37701a24ef6d720208
piyush singh tanwar <piyushsinghtomar777@gmail.com>
2026-06-29 14:49:50 +0530

    fix: request instagram_content_publish permission and revert worker ID

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -85,7 +85,7 @@ export default function Settings() {
       },
       {
         scope:
-          'instagram_basic,instagram_manage_messages,instagram_manage_comments,pages_messaging,pages_read_engagement,pages_show_list,business_management',
+          'instagram_basic,instagram_manage_messages,instagram_manage_comments,instagram_content_publish,pages_messaging,pages_read_engagement,pages_show_list,business_management',
       }
     );
   };
